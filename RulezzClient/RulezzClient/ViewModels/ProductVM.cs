53cf9cb59566db9f9956f49b4afd017f7139e391
Demertal <36508077+Demertal@users.noreply.github.com>
2019-01-27 11:05:16 +0300

    Create ProductVM.cs

---
+++ RulezzClient/RulezzClient/ViewModels/ProductVM.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+using RulezzClient.Model;
+
+namespace RulezzClient.ViewModels
+{
+    class ProductListVm : BindableBase
+    {
+        private readonly ObservableCollection<Product> _productList =
+            new ObservableCollection<Product>();
+
+        public ReadOnlyObservableCollection<Product> Products;
+
+        public ProductListVm()
+        {
+            Products = new ReadOnlyObservableCollection<Product>(_productList);
+        }
+
+        public async Task<List<Product>> GetListProduct(int idNomenclatureSubgroup)
+        {
+            List<Product> tempM =
+                await Task.Run(() => Product.AsyncLoad(Properties.Settings.Default.СconnectionString, idNomenclatureSubgroup));
+            if (tempM == null){ _productList.Clear(); return null;}
+            _productList.Clear();
+            foreach (var t in tempM)
+            {
+                _productList.Add(t);
+            }
+            return tempM;
+        }
+    }
+}
