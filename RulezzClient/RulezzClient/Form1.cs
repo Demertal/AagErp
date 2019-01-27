ea4ca919370edf15e9b694cbca3368b994246195
Demertal <36508077+Demertal@users.noreply.github.com>
2019-01-27 11:06:56 +0300

    Update Form1.cs

--- RulezzClient/RulezzClient/Form1.cs
+++ RulezzClient/RulezzClient/Form1.cs
@@ -5,18 +5,18 @@ namespace RulezzClient
 {
     public partial class Form1 : Form
     {
-        private MainWindow _mw;
+        //private Views.MainWindow _mw;
 
-        public Form1(MainWindow mw)
-        {
-            _mw = mw;
-            InitializeComponent();
-        }
+        //public Form1(Views.MainWindow mw)
+        //{
+        //    _mw = mw;
+        //    InitializeComponent();
+        //}
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            _mw.NameServer = textBox1.Text;
-            Close();
-        }
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+        //    _mw.NameServer = textBox1.Text;
+        //    Close();
+        //}
     }
 }
