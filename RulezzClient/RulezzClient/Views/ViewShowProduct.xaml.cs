1727042ce4830f9a843174f7551dbcd7fea3da3e
Demertal <36508077+Demertal@users.noreply.github.com>
2019-01-27 11:04:23 +0300

    Create ViewShowProduct.xaml.cs

---
+++ RulezzClient/RulezzClient/Views/ViewShowProduct.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace RulezzClient.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для ViewShowProduct.xaml
+    /// </summary>
+    public partial class ViewShowProduct : UserControl
+    {
+        public ViewShowProduct()
+        {
+            InitializeComponent();
+        }
+    }
+}
