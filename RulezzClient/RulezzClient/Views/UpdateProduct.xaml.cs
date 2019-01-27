adbfba2c161b3a20073ce0e8f6f549aa35579aa2
Demertal <36508077+Demertal@users.noreply.github.com>
2019-01-27 11:04:33 +0300

    Create UpdateProduct.xaml.cs

---
+++ RulezzClient/RulezzClient/Views/UpdateProduct.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace RulezzClient.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для UpdateProduct.xaml
+    /// </summary>
+    public partial class UpdateProduct : UserControl
+    {
+        public UpdateProduct()
+        {
+            InitializeComponent();
+        }
+    }
+}
