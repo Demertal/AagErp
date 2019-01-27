1b1a110772a727364dedc3d987b5719480ad5422
Demertal <36508077+Demertal@users.noreply.github.com>
2019-01-27 11:06:25 +0300

    Update Settings.Designer.cs

--- RulezzClient/RulezzClient/Properties/Settings.Designer.cs
+++ RulezzClient/RulezzClient/Properties/Settings.Designer.cs
@@ -34,5 +34,17 @@ namespace RulezzClient.Properties {
                 this["СconnectionString"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("-1")]
+        public int SelectedStoreID {
+            get {
+                return ((int)(this["SelectedStoreID"]));
+            }
+            set {
+                this["SelectedStoreID"] = value;
+            }
+        }
     }
 }
