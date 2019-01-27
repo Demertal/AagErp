bf7dff0d709d0eed6b18b4dc3b24e2305c0ef95c
Demertal <36508077+Demertal@users.noreply.github.com>
2019-01-27 11:05:14 +0300

    Create ShellViewModel.cs

---
+++ RulezzClient/RulezzClient/ViewModels/ShellViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System.Windows;
+using Prism.Commands;
+using Prism.Mvvm;
+
+namespace RulezzClient.ViewModels
+{
+    class ShellViewModel : BindableBase
+    {
+        private readonly IUiDialogService _dialogService = new DialogService();
+        private Visibility _isProductGroupVisible;
+        private ShowProductViewModel _showProduct;
+
+        public ShellViewModel()
+        {
+            IsProductGroupVisible = Visibility.Collapsed;
+            ShowProductCommand = new DelegateCommand(() =>
+            {
+                IsProductGroupVisible = Visibility.Visible;
+                ShowProduct = new ShowProductViewModel();
+            });
+            AddProductMainCommand = new DelegateCommand(() =>
+            {
+                AddProductViewModel viewModel = new AddProductViewModel();
+                _dialogService.ShowDialog(DialogService.ChoiceView.AddProduct, viewModel, true, b => { });
+                if (IsProductGroupVisible == Visibility.Visible) ShowProduct.Update(ShowProductViewModel.ChoiceUpdate.Product);
+            });
+        }
+
+        public Visibility IsProductGroupVisible
+        {
+            get => _isProductGroupVisible;
+            set
+            {
+                _isProductGroupVisible = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public ShowProductViewModel ShowProduct
+        {
+            get => _showProduct;
+            set
+            {
+                _showProduct = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public DelegateCommand ShowProductCommand { get; }
+
+        public DelegateCommand AddProductMainCommand { get; }
+    }
+}
