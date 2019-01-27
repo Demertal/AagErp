6acf73def4c46322ae4e9b04a07f3852042e0b39
Demertal <36508077+Demertal@users.noreply.github.com>
2019-01-27 11:06:52 +0300

    Create IUiDialogService.cs

---
+++ RulezzClient/RulezzClient/IUiDialogService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+
+namespace RulezzClient
+{
+    public interface IUiDialogService
+    {
+        void ShowDialog(object view, object dataContext, bool isModal, Action<bool?> closeAction);
+
+        MessageBoxResult ShowMessageBox(
+            string messageBoxText,
+            string caption,
+            MessageBoxButton button,
+            MessageBoxImage icon);
+
+        void ShowMessageBox(
+            string messageBoxText,
+            string caption,
+            MessageBoxButton button,
+            MessageBoxImage icon,
+            Action<MessageBoxResult> responseResult);
+    }
+}
